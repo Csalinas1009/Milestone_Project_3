c05bfe1e6d15fc528e1dfcadd5e516af94364f58
Christian Salinas <ChristianSalinas1009@gmail.com>
2023-01-07 14:49:06 -0500

    setting up initial Cards for Gallery component

--- client/front-end/src/components/App.js
+++ client/front-end/src/components/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navigation from './Navigation'
+import Gallery from './Gallery';
 import '../styles/styles.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div>
-      <Navigation/>
-    </div>
+    <><Navigation /><div>
+      <Gallery />
+    </div></>
   )
 }
 
