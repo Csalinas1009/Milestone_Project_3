e193c961aeeec1e03525ebc452b942906dba5072
Gio <103082092+Gioperes@users.noreply.github.com>
2023-01-08 12:36:03 -0500

    Add files via upload

---
+++ Milestone_Project_3-Authentication/passport.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const findOrCreate = require("mongoose-findorcreate");
+
+var GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+passport.use(new GoogleStrategy({
+    clientID: "200690232924-bgf4n18dh9939b6hdg8glsph28eki3nu.apps.googleusercontent.com",
+    clientSecret: "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",
+    callbackURL: "http://localhost4000/auth/google/callback"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile'] }));
+
+app.get('/auth/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
